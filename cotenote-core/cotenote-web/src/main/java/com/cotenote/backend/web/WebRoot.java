d4bd6ba09b982f0a2de553122538656b15b52f20
HelloMinchan <wjdalscksdle@naver.com>
2023-03-07 02:19:02 +0900

    feat(core): add web module

---
+++ cotenote-core/cotenote-web/src/main/java/com/cotenote/backend/web/WebRoot.java
@@ -0,0 +1,10 @@
+package com.cotenote.backend.web;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan(basePackageClasses = WebRoot.class)
+@Configuration
+public class WebRoot {
+
+}
